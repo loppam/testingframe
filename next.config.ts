68e688c24632c7194311d503f2cc570e58a26d73
loppam <lolade132@gmail.com>
2025-03-13 08:45:50 +0100

    Hopeful Fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ["testingframe-iota.vercel.app"],
+  },
+  env: {
+    NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
+  },
 };
 
 export default nextConfig;
